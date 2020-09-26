abfc0a5b26bcb1653be21701309cd7c2283a4bc2
Isaac Curtiss <ijcurtiss7@gmail.com>
2020-09-26 19:52:00 -0400

    add more tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -3,7 +3,7 @@ import { KeywordInTweet } from '../index';
 describe('KeywordInTweet with a normal keyword', () => {
   test("should be able to tell when the exact text of a word is in a tweet's text", () => {
     const tweetOne = {
-      text: '@John http:bark.com burk this was ...barkk barksalot abark! #bark fun game @bark',
+      text: '@John http:bark.com !bark! this was ...barkk barksalot abark! #bark fun game @bark',
       keyword: 'bark',
       result: false,
     };
@@ -14,39 +14,39 @@ describe('KeywordInTweet with a normal keyword', () => {
       result: true,
     };
 
+    const tweetThree = {
+      text: '@John http:bark.com burk this was ...barkk barksalot abark! #bark fun game @bark',
+      keyword: 'bark',
+      result: false,
+    };
+
     expect(KeywordInTweet(tweetOne.text, tweetOne.keyword)).toBe(tweetOne.result);
     expect(KeywordInTweet(tweetTwo.text, tweetTwo.keyword)).toBe(tweetTwo.result);
+    expect(KeywordInTweet(tweetThree.text, tweetThree.keyword)).toBe(tweetThree.result);
   });
 });
 
 describe('KeywordInTweet with a keyword surrounded by characters', () => {
   test("should be able to tell when that word (with the characters) is in a tweet's text", () => {
-    const tweetThree = {
+    const tweetFour = {
       text: '@John http:bark.com !barkk! this was ...barkk barksalot abark! #bark fun game @bark',
       keyword: '!bark!',
       result: false,
     };
 
-    const tweetFour = {
+    const tweetFive = {
       text: '@John http:bark.com !bark! this was ...barkk barksalot abark! #bark fun game @bark',
       keyword: '!bark!',
       result: true,
     };
 
-    expect(KeywordInTweet(tweetThree.text, tweetThree.keyword)).toBe(tweetThree.result);
     expect(KeywordInTweet(tweetFour.text, tweetFour.keyword)).toBe(tweetFour.result);
+    expect(KeywordInTweet(tweetFive.text, tweetFive.keyword)).toBe(tweetFive.result);
   });
 });
 
 describe('KeywordInTweet with a keyword that is spaced out', () => {
   test("should be able to tell when that word (with the characters) is in a tweet's text", () => {
-    const tweetFive = {
-      text:
-        '@John http:bark.com ! b a r k k! this was ...barkk barksalot abark! #bark fun game @bark',
-      keyword: '!bark!',
-      result: false,
-    };
-
     const tweetSix = {
       text:
         '@John http:bark.com ! b a r k ! this was ...barkk barksalot abark! #bark fun game @bark',
@@ -61,8 +61,15 @@ describe('KeywordInTweet with a keyword that is spaced out', () => {
       result: true,
     };
 
-    expect(KeywordInTweet(tweetFive.text, tweetFive.keyword)).toBe(tweetFive.result);
+    const tweetEight = {
+      text:
+        '@John http:bark.com ! b a r k k! this was ...barkk barksalot abark! #bark fun game @bark',
+      keyword: '!bark!',
+      result: false,
+    };
+
     expect(KeywordInTweet(tweetSix.text, tweetSix.keyword)).toBe(tweetSix.result);
     expect(KeywordInTweet(tweetSeven.text, tweetSeven.keyword)).toBe(tweetSeven.result);
+    expect(KeywordInTweet(tweetEight.text, tweetEight.keyword)).toBe(tweetEight.result);
   });
 });
